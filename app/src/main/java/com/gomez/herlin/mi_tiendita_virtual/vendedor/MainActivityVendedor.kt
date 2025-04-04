a71c45e962f070f41a02c93a46df926d62742f4c
Herlin Gomez <hgomez@bytesw.com>
2025-04-04 14:40:54 -0600

    se crean los elementos de los navbar de vendedor

---
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/vendedor/MainActivityVendedor.kt
@@ -0,0 +1,13 @@
+package com.gomez.herlin.mi_tiendita_virtual.vendedor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.gomez.herlin.mi_tiendita_virtual.R
+
+class MainActivityVendedor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_vendedor)
+    }
+}
