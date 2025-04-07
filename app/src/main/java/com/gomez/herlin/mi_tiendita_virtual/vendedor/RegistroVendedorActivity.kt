3355fa455ad61e43f8daf29cc626488afa3f9b9c
Herlin Gomez <hgomez@bytesw.com>
2025-04-07 11:51:20 -0600

    Se crea la vista de registro de vendedor

---
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,12 @@
+package com.gomez.herlin.mi_tiendita_virtual.vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gomez.herlin.mi_tiendita_virtual.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+    }
+}
