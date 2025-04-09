f5b9c9bef32156f1847314748c1e106095211236
Herlin Gomez <hgomez@bytesw.com>
2025-04-09 10:51:15 -0600

    Se cren los menu nav y bottomBar para la vista de clientes y se agregan iconos para estos menus

--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/MainActivityCliente.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/MainActivityCliente.kt
@@ -1,16 +1,17 @@
-package com.gomez.herlin.mi_tiendita_virtual
+package com.gomez.herlin.mi_tiendita_virtual.cliente
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.gomez.herlin.mi_tiendita_virtual.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_cliente)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
