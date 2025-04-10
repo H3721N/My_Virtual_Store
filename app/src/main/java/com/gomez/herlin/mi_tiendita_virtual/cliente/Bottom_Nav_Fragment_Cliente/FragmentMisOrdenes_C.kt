91eba8507e087aff074dfb7cf6aa88dbf47e0214
Herlin Gomez <hgomez@bytesw.com>
2025-04-10 09:20:02 -0600

    Se agregan las vistas del menu de cliente

---
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Bottom_Nav_Fragment_Cliente/FragmentMisOrdenes_C.kt
@@ -0,0 +1,20 @@
+package com.gomez.herlin.mi_tiendita_virtual.cliente.Bottom_Nav_Fragment_Cliente
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gomez.herlin.mi_tiendita_virtual.R
+
+class FragmentMisOrdenes_C : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mis_ordenes_c, container, false)
+    }
+
+}
