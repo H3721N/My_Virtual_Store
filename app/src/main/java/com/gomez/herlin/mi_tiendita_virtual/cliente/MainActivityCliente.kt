b0b9bad5ef00ce02ccccd58677105b97718c0971
Herlin Gomez <hgomez@bytesw.com>
2025-04-10 12:17:19 -0600

    Se aplica navegacion a la vista inicial ce cliente

--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/MainActivityCliente.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/MainActivityCliente.kt
@@ -1,21 +1,79 @@
 package com.gomez.herlin.mi_tiendita_virtual.cliente
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gomez.herlin.mi_tiendita_virtual.R
+import com.gomez.herlin.mi_tiendita_virtual.databinding.ActivityMainClienteBinding
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.gomez.herlin.mi_tiendita_virtual.cliente.Bottom_Nav_Fragment_Cliente.FragmentMisOrdenes_C
+import com.gomez.herlin.mi_tiendita_virtual.cliente.Nav_Fragments_Cliente.FragmentInicioC
+import com.gomez.herlin.mi_tiendita_virtual.cliente.Nav_Fragments_Cliente.FragmentMiPerfilC
 
-class MainActivityCliente : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding : ActivityMainClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main_cliente)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toogle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        replaceceFragment(FragmentInicioC())
     }
+
+    private fun replaceceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment, fragment)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.op_inicio_v -> {
+                replaceceFragment(FragmentInicioC())
+            }
+            R.id.op_mis_ordenes_c -> {
+                replaceceFragment(FragmentMisOrdenes_C())
+            }
+            R.id.op_mi_perfil_c -> {
+                replaceceFragment(FragmentMiPerfilC())
+            }
+            R.id.nav_cerrar_sesion_c -> {
+                Toast.makeText(this, "Cerrar Sesion", Toast.LENGTH_SHORT).show()
+            }
+            R.id.op_mi_tienda_c -> {
+                Toast.makeText(this, "Mi Tienda", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Nav_Fragments_Cliente/FragmentInicioC.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Nav_Fragments_Cliente/FragmentInicioC.kt
@@ -6,16 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gomez.herlin.mi_tiendita_virtual.R
+import com.gomez.herlin.mi_tiendita_virtual.cliente.Bottom_Nav_Fragment_Cliente.FragmentMisOrdenes_C
+import com.gomez.herlin.mi_tiendita_virtual.cliente.Bottom_Nav_Fragment_Cliente.FragmentTiendaC
+import com.gomez.herlin.mi_tiendita_virtual.databinding.FragmentInicioCBinding
 
 class FragmentInicioC : Fragment() {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    private lateinit var binding: FragmentInicioCBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_c, container, false)
+        binding = FragmentInicioCBinding.inflate(inflater, container, false)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.op_mi_tienda_c -> {
+                    replaceceFragment(FragmentTiendaC())
+                }
+                R.id.op_mis_ordenes_c -> {
+                    replaceceFragment(FragmentMisOrdenes_C())
+                }
+            }
+            true
+        }
+
+        replaceceFragment(FragmentTiendaC())
+        binding.bottomNavigation.selectedItemId = R.id.op_mi_tienda_c
+
+        return binding.root
+    }
+
+    private fun replaceceFragment(fragment: Fragment) {
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.bottomFragment, fragment)
+            .commit()
     }
 
 }
