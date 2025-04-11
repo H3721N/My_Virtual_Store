81e8ccd8967a27c8d40e159f184d2b7c3558e707
Herlin Gomez <hgomez@bytesw.com>
2025-04-11 14:30:12 -0600

    Se comprueba si el usuario ha iniciado sesion y se agrega la funcionalidad de cerrar sesion

--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/MainActivityCliente.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/MainActivityCliente.kt
@@ -1,5 +1,6 @@
 package com.gomez.herlin.mi_tiendita_virtual.cliente
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
@@ -15,21 +16,26 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.gomez.herlin.mi_tiendita_virtual.SeleccionarTipoActivity
 import com.gomez.herlin.mi_tiendita_virtual.cliente.Bottom_Nav_Fragment_Cliente.FragmentMisOrdenes_C
 import com.gomez.herlin.mi_tiendita_virtual.cliente.Nav_Fragments_Cliente.FragmentInicioC
 import com.gomez.herlin.mi_tiendita_virtual.cliente.Nav_Fragments_Cliente.FragmentMiPerfilC
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivityCliente : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding : ActivityMainClienteBinding
+    private var firebaseAuth : FirebaseAuth?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_cliente)
         setContentView(binding.root)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        comprobarSesion()
 
         binding.navigationView.setNavigationItemSelectedListener(this)
 
@@ -47,11 +53,28 @@ class MainActivityCliente : AppCompatActivity() , NavigationView.OnNavigationIte
         replaceceFragment(FragmentInicioC())
     }
 
+    private fun comprobarSesion() {
+        if (firebaseAuth!!.currentUser==null) {
+            startActivity(Intent(this, SeleccionarTipoActivity::class.java))
+            finishAffinity()
+        }
+        else {
+            Toast.makeText(this, getString(R.string.usuarioLinea), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun replaceceFragment(fragment: Fragment) {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.navFragment, fragment)
             .commit()
+    }
+
+    private fun cerrarSesion() {
+        firebaseAuth!!.signOut()
+        startActivity(Intent(this@MainActivityCliente, SeleccionarTipoActivity::class.java))
+        finishAffinity()
+        Toast.makeText(this, getString(R.string.cerrarSesion), Toast.LENGTH_SHORT).show()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -66,7 +89,7 @@ class MainActivityCliente : AppCompatActivity() , NavigationView.OnNavigationIte
                 replaceceFragment(FragmentMiPerfilC())
             }
             R.id.nav_cerrar_sesion_c -> {
-                Toast.makeText(this, "Cerrar Sesion", Toast.LENGTH_SHORT).show()
+                cerrarSesion()
             }
             R.id.op_mi_tienda_c -> {
                 Toast.makeText(this, "Mi Tienda", Toast.LENGTH_SHORT).show()
