53d06b28aa14e65f74de32d3889aecec2cf21159
Herlin Gomez <hgomez@bytesw.com>
2025-04-11 12:56:49 -0600

    se agrega la funcionalidad de navegacion hacia RegistroClientesActivity

--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/LoginClienteActivity.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/LoginClienteActivity.kt
@@ -1,12 +1,29 @@
 package com.gomez.herlin.mi_tiendita_virtual.cliente
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gomez.herlin.mi_tiendita_virtual.R
+import com.gomez.herlin.mi_tiendita_virtual.databinding.ActivityLoginClienteBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginClienteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginClienteBinding
+    private lateinit var  firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_cliente)
+        binding = ActivityLoginClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvRegistrarC.setOnClickListener {
+            startActivity(Intent(this@LoginClienteActivity, RegistroClientesActivity::class.java))
+        }
+
+
     }
 }
