08d5bac2068876d97f17e6773b206129e2fbac7a
Herlin Gomez <hgomez@bytesw.com>
2025-04-11 09:40:41 -0600

    Se corrigen errores de registro vendedor

---
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Nav_Fragments_Cliente/RegistroClientesActivity.kt
@@ -0,0 +1,12 @@
+package com.gomez.herlin.mi_tiendita_virtual.cliente.Nav_Fragments_Cliente
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gomez.herlin.mi_tiendita_virtual.R
+
+class RegistroClientesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_clientes)
+    }
+}
