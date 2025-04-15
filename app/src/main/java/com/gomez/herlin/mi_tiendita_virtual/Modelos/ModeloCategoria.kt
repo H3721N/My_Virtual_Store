14b935626554d8b37f94ccd7bfa0f35384ffcefd
Herlin Gomez <hgomez@bytesw.com>
2025-04-15 11:39:40 -0600

    Se agrega un imput para agregar categorias existentes al producto

---
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/Modelos/ModeloCategoria.kt
@@ -0,0 +1,13 @@
+package com.gomez.herlin.mi_tiendita_virtual.Modelos
+
+class ModeloCategoria {
+    var id : String = ""
+    var categoria : String = ""
+
+    constructor() {}
+
+    constructor(id: String, categoria: String) {
+        this.id = id
+        this.categoria = categoria
+    }
+}
--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/vendedor/AgregarProductoActivity.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/vendedor/AgregarProductoActivity.kt
@@ -1,6 +1,7 @@
 package com.gomez.herlin.mi_tiendita_virtual.vendedor
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
@@ -10,9 +11,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.gomez.herlin.mi_tiendita_virtual.Adaptadores.AdaptadorIMagenSeleccionada
 import com.gomez.herlin.mi_tiendita_virtual.Constantes
+import com.gomez.herlin.mi_tiendita_virtual.Modelos.ModeloCategoria
 import com.gomez.herlin.mi_tiendita_virtual.Modelos.ModeloImagenSeleccionada
 import com.gomez.herlin.mi_tiendita_virtual.R
 import com.gomez.herlin.mi_tiendita_virtual.databinding.ActivityAgregarProductoBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AgregarProductoActivity : AppCompatActivity() {
 
@@ -22,10 +28,14 @@ class AgregarProductoActivity : AppCompatActivity() {
     private lateinit var imageSelecArrayList: ArrayList<ModeloImagenSeleccionada>
     private lateinit var adaptadorIMagenSel: AdaptadorIMagenSeleccionada
 
+    private lateinit var categoriaArrayList : ArrayList<ModeloCategoria>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarProductoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        cargarCategorias()
 
         imageSelecArrayList = ArrayList()
 
@@ -33,7 +43,47 @@ class AgregarProductoActivity : AppCompatActivity() {
             seleccionarImg()
         }
 
+        binding.Categoria.setOnClickListener {
+            selecCategorias()
+        }
+
         cargarImagenes()
+    }
+
+    private fun cargarCategorias() {
+        categoriaArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Categorias").orderByChild("categoria")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                categoriaArrayList.clear()
+                for (ds in snapshot.children) {
+                    val modelo = ds.getValue(ModeloCategoria::class.java)
+                    categoriaArrayList.add(modelo!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private var idCet = ""
+    private var tituloCat = ""
+
+    private fun selecCategorias () {
+        val categoriasArry = arrayOfNulls<String>(categoriaArrayList.size)
+        for (i in categoriaArrayList.indices) {
+            categoriasArry[i] = categoriaArrayList[i].categoria
+        }
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.seleccionar_categoria))
+            .setItems(categoriasArry) { dialog, which ->
+                idCet = categoriaArrayList[which].id
+                tituloCat = categoriaArrayList[which].categoria
+                binding.Categoria.text = tituloCat
+            }.show()
     }
 
     private fun cargarImagenes() {
