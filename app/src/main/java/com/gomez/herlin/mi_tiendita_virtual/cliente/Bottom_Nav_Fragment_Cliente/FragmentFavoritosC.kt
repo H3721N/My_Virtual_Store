afd79a4549a807f8bee1d2d4638e4053f3f0ae79
Herlin Gomez <hgomez@bytesw.com>
2025-04-24 15:59:25 -0600

    Funcion para listar los favoritos

--- app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Bottom_Nav_Fragment_Cliente/FragmentFavoritosC.kt
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Bottom_Nav_Fragment_Cliente/FragmentFavoritosC.kt
@@ -1,19 +1,89 @@
 package com.gomez.herlin.mi_tiendita_virtual.cliente.Bottom_Nav_Fragment_Cliente
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.gomez.herlin.mi_tiendita_virtual.Adaptadores.AdaptadorProductoC
+import com.gomez.herlin.mi_tiendita_virtual.Modelos.ModeloProducto
 import com.gomez.herlin.mi_tiendita_virtual.R
+import com.gomez.herlin.mi_tiendita_virtual.databinding.FragmentFavoritosCBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class FragmentFavoritosC : Fragment() {
 
+    private lateinit var binding: FragmentFavoritosCBinding
+    private lateinit var mContext: Context
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var productosArrayList: ArrayList<ModeloProducto>
+    private lateinit var productosAdaptador: AdaptadorProductoC
+
+    override fun onAttach(context: Context) {
+        this.mContext = context
+        super.onAttach(context)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favoritos_c, container, false)
+        binding = FragmentFavoritosCBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        cargarProdFav()
+    }
+
+    private fun cargarProdFav() {
+        productosArrayList = ArrayList()
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(firebaseAuth.uid!!).child("Favoritos")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    productosArrayList.clear()
+                    for (ds in snapshot.children) {
+                        val idProducto = "${ds.child("idProducto").value}"
+
+                        val refProd = FirebaseDatabase.getInstance().getReference("Productos")
+                        refProd.child(idProducto)
+                            .addValueEventListener(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    try {
+                                        val modeloProducto = snapshot.getValue(ModeloProducto::class.java)
+                                        productosArrayList.add(modeloProducto!!)
+                                    }catch (e: Exception){
+                                        e.printStackTrace()
+                                    }
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+                                    TODO("Not yet implemented")
+                                }
+
+                            })
+                    }
+                    Handler().postDelayed({
+                        productosAdaptador = AdaptadorProductoC(mContext, productosArrayList)
+                        binding.favoritosRV.adapter = productosAdaptador
+                    }, 500)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
     }
 
 }
