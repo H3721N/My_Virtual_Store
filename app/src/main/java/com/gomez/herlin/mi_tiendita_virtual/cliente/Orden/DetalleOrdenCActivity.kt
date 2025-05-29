cbe66f3f66faf424f65dd3a9daa4b79a85ebc963
hgomez <hgomez@bytesw.com>
2025-05-29 17:08:12 -0600

    Se crea la vista que muestra los detalles de las ordenes

---
+++ app/src/main/java/com/gomez/herlin/mi_tiendita_virtual/cliente/Orden/DetalleOrdenCActivity.kt
@@ -0,0 +1,12 @@
+package com.gomez.herlin.mi_tiendita_virtual.cliente.Orden
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gomez.herlin.mi_tiendita_virtual.R
+
+class DetalleOrdenCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_orden_cactivity)
+    }
+}
